45aa0a01eda5661b6635d91f618c9bcb2694c2e5
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-13 15:31:57 +0800

    get box on image

---
+++ components/docker/SPImageBox.py
@@ -0,0 +1,86 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import os
+import json
+from suanpan.app import app
+from suanpan.app.arguments import Folder, ListOfInt, Int
+from suanpan.log import logger
+from suanpan.utils import image
+from suanpan.storage import storage
+from arguments import Images
+
+
+@app.input(Images(key="inputImage"))
+@app.input(Folder(key="inputData"))
+@app.param(ListOfInt(key="xy", default=[10, 10]))
+@app.param(Int(key="x", default=200))
+@app.param(Int(key="y", default=200))
+@app.output(Images(key="outputImage"))
+def SPImageBox(context):
+    args = context.args
+    images = args.inputImage
+
+    outputData = []
+    files = []
+
+    try:
+        if args.inputData:
+            jsonFile = os.path.join(args.inputData, "project.json")
+            with open(jsonFile, "rb") as load_f:
+                fileInfo = json.load(load_f)
+
+            for i, j in fileInfo["metadata"].items():
+                files.append(os.path.join(images.folder, j["vid"]))
+                try:
+                    filename = (
+                        os.path.splitext(j["vid"])[0] + "_" + j["av"]["1"] + ".png"
+                    )
+                except:
+                    filename = (
+                        os.path.splitext(j["vid"])[0] + "_" + i.split("_")[-1] + ".png"
+                    )
+                xy = j["xy"][1:]
+                img = image.read(os.path.join(images.folder, j["vid"]))
+                outputData.append(
+                    (
+                        filename,
+                        img[
+                            int(xy[1]) : int(xy[1] + xy[3]),
+                            int(xy[0]) : int(xy[0] + xy[2]),
+                            :,
+                        ],
+                    )
+                )
+        elif args.xy:
+            for idx, img in enumerate(images):
+                files.append(images.images[idx])
+                x = int(args.xy[0] + args.x) if args.x else img.shape[1] + 1
+                y = int(args.xy[1] + args.y) if args.y else img.shape[0] + 1
+                outputData.append(
+                    (
+                        storage.delimiter.join(
+                            images.images[idx].split(storage.delimiter)[8:]
+                        ),
+                        img[int(args.xy[1]) : y, int(args.xy[0]) : x, :,],
+                    )
+                )
+    except:
+        logger.info("can not find project.json or json format error")
+
+    for idx, img in enumerate(images):
+        if images.images[idx] not in files:
+            outputData.append(
+                (
+                    storage.delimiter.join(
+                        images.images[idx].split(storage.delimiter)[8:]
+                    ),
+                    img,
+                )
+            )
+
+    return outputData
+
+
+if __name__ == "__main__":
+    SPImageBox()
