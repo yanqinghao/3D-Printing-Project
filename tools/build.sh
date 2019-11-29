b6412ed16fa562e7de36ed7e1ceec52c4d37a978
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-29 10:55:06 +0800

    build

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,9 @@
-# docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker-gpu:$1 -f docker/docker_3dprinting_gpu/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker-gpu:$1 -f docker/docker_3dprinting_gpu/Dockerfile .
 docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker:$1 -f docker/docker_3dprinting/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-stream-gpu:$1 -f docker/stream_3dprinting_gpu/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-stream:$1 -f docker/stream_3dprinting/Dockerfile .
 
 docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker:$1
-# docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker-gpu:$1
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-docker-gpu:$1
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-stream:$1
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/3dprinting-stream-gpu:$1
